d84aa0758720ad036b3bd842ac28fc9b4b9c8f79
razisek <agusdono27@gmail.com>
2023-07-14 11:23:40 +0700

    add overview

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,12 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard');
+        $user = auth()->user();
+        $balance = $user->page->balance->balance ?? 0;
+        $transactions = $user->page->transactions->where('payment_status', 'paid')->count();
+        $posts = $user->posts->count();
+        $unit = $user->page->unit;
+
+        return view('dashboard.dashboard', compact('balance', 'transactions', 'posts', 'unit'));
     }
 }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -7,20 +7,20 @@
         <p class="text-md pt-2">Berikut adalah informasi tentang semua Jajanin Anda</p>
         <div class="p-6 bg-white border border-gray-200 rounded-lg shadow mt-8 grid grid-cols-4 divide-x-2">
             <div class="pl-6">
-                <p class="font-semibold text-primary text-2xl">Rp100.000</p>
-                <p class="text-secondary text-md pt-2">Jumlah saat ini</p>
+                <p class="font-semibold text-primary text-2xl">Rp {{ number_format($balance, 0, ',', '.') }}</p>
+                <p class="text-secondary text-md pt-2">Jumlah saldo saat ini</p>
             </div>
             <div class="pl-6">
-                <p class="font-semibold text-primary text-2xl">Rp100.000</p>
-                <p class="text-secondary text-md pt-2">Jumlah saat ini</p>
+                <p class="font-semibold text-primary text-2xl">{{ $transactions }} Supporter</p>
+                <p class="text-secondary text-md pt-2">Jumlah support saat ini</p>
             </div>
             <div class="pl-6">
-                <p class="font-semibold text-primary text-2xl">Rp100.000</p>
-                <p class="text-secondary text-md pt-2">Jumlah saat ini</p>
+                <p class="font-semibold text-primary text-2xl">{{ $posts }} Post</p>
+                <p class="text-secondary text-md pt-2">Jumlah post saat ini</p>
             </div>
             <div class="pl-6">
-                <p class="font-semibold text-primary text-2xl">Rp100.000</p>
-                <p class="text-secondary text-md pt-2">Jumlah saat ini</p>
+                <p class="font-semibold text-primary text-2xl">{{ $unit->name }} - Rp {{ number_format($unit->price, 0, ',', '.') }}</p>
+                <p class="text-secondary text-md pt-2">Unit aktif</p>
             </div>
         </div>
     </div>
